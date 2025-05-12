4ddbc89f0ebfbd59d3d5475706c61fdacc442eec
elizanierwinska <nierwinskaeliza@gmail.com>
2025-05-12 22:46:56 +0200

    Ch2 L3 - Read File

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+import unittest
+
+def get_book_text(filepath):
+    text = ""
+    with open(filepath) as f:
+        text = f.read()
+    return text
+
+def main():
+   print(get_book_text("./books/frankenstein.txt"))
+
+class TestGetBookTextFunction(unittest.TestCase):
+    def test_null_input(self):
+        self.assertEqual(get_book_text(""), FileNotFoundError)
+
+if __name__ == "__main__":
+    main()
