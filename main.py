377cced34fa399a0d461517c027c322fb16c79cd
elizanierwinska <nierwinskaeliza@gmail.com>
2025-05-14 22:58:26 +0200

    Ch3 L1 - Print a report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_num_words
 from stats import get_letters
+from stats import sorted_list
+from stats import sort_on
 
 def get_book_text(filepath):
     text = ""
@@ -8,9 +10,19 @@ def get_book_text(filepath):
     return text 
 
 def main():
+    path = "books/frankenstein.txt"
+    list = sorted_list(get_letters(get_book_text("./books/frankenstein.txt")))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
     num_words = get_num_words(get_book_text("./books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    print(get_letters(get_book_text("./books/frankenstein.txt")))
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    list.sort(reverse=True, key=sort_on)
+    for l in list:
+        char = l['char']
+        num = l['num']
+        print(f'{char}: {num}')
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -14,6 +14,19 @@ def get_letters(array):
             else:
                 dict[l] = 1
     return dict
+
+def sorted_list(dict):
+    list = []
+    for d in dict:
+        if d.isalpha():
+            temp = {}
+            temp["char"] = d
+            temp["num"] = dict[d]
+            list.append(temp)
+    return list
+
+def sort_on(dict):
+    return dict["num"]
 # class TestGetBookTextFunction(unittest.TestCase):
 #     def test_null_input(self):
 #         self.assertEqual(get_book_text(""), FileNotFoundError)
