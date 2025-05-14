a78d50691742eb0e20c4949897f0b47804e88f69
elizanierwinska <nierwinskaeliza@gmail.com>
2025-05-14 22:35:43 +0200

    Ch2 L6 - Count Characters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_letters
 
 def get_book_text(filepath):
     text = ""
@@ -9,6 +10,7 @@ def get_book_text(filepath):
 def main():
     num_words = get_num_words(get_book_text("./books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
+    print(get_letters(get_book_text("./books/frankenstein.txt")))
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -4,6 +4,16 @@ def get_num_words(book):
     text = book.split()
     return len(text)
 
+def get_letters(array):
+    dict = {}
+    for word in array:
+        lowercase = word.lower()
+        for l in lowercase:
+            if l in dict:
+                dict[l] += 1
+            else:
+                dict[l] = 1
+    return dict
 # class TestGetBookTextFunction(unittest.TestCase):
 #     def test_null_input(self):
 #         self.assertEqual(get_book_text(""), FileNotFoundError)
